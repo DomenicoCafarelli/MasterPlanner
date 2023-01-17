c408121a897e7a2c3235716df09771a98f60248a
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-17 13:51:41 +0100

    created the send notification function

--- Master Planner/Master Planner/ContentView.swift
+++ Master Planner/Master Planner/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import UserNotifications
+
 
 // Model to hold the Todo data
 struct Todo: Identifiable {
@@ -18,6 +20,21 @@ struct Todo: Identifiable {
 // State to hold the Todo data
 class TodoList: ObservableObject {
     @Published var todos: [Todo] = []
+    
+    func scheduleNotification(for todo: Todo) {
+        let content = UNMutableNotificationContent()
+        content.title = "Task Reminder"
+        content.body = todo.task
+        content.sound = UNNotificationSound.default
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: todo.deadline)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        let request = UNNotificationRequest(identifier: todo.id.uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error scheduling notification: \(error)")
+            }
+        }
+    }
 }
 
 
